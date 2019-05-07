4c85f431d5993e6f45dd36faf6bd383844e38908
Sunil Kansodiya <sunil.kansodiya@rishabhsoft.com>
2019-05-07 14:36:24 +0530

    Commit on second branch.

--- fundscape_calculations.php
+++ fundscape_calculations.php
@@ -23,7 +23,7 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       fundscape_calculations
  * Domain Path:       /languages
- */
+ */       
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
